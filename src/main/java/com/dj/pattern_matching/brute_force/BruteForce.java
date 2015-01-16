4f15bf2699cbc441b81d27f5bf6c0b26b51e39ac
djsecret <djsecret123@gmail.com>
2015-01-16 15:18:11 +0800

    add Brute Force Algorithm

---
+++ src/main/java/com/dj/pattern_matching/brute_force/BruteForce.java
@@ -0,0 +1,11 @@
+package com.dj.pattern_matching.brute_force;
+
+/**
+ * 蛮力算法检查数据串从0到n-m的每个位置，不管模式是否出现在那里。每次比较过后，将模式串向右移动一个位置。<br/>
+ *  时间复杂度为O(mn)，空间复杂度为O(m+n)，比较字符数的期望为2n。<br/>
+ * Created by dj on 15-1-16.
+ */
+public class BruteForce
+{
+
+}
